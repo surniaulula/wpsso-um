559a6515c84834f168a6e65b9da21de2ab10c18d
JS Morisset <jsm@surniaulula.com>
2021-09-20 12:25:20 -0700

    v4.6.0-dev.7

--- gettext/gettext-lib-config.php
+++ gettext/gettext-lib-config.php
@@ -4,7 +4,7 @@
  * Extract of translatable strings from lib/config.php.
  */
 _x( 'WPSSO Schema Shortcode', 'plugin name', 'wpsso-um' );
-_x( 'Schema shortcode for advanced users.', 'plugin description', 'wpsso-um' );
+_x( 'Shortcode for advanced Schema markup of content sections.', 'plugin description', 'wpsso-um' );
 _x( 'Update Manager', 'lib file description', 'wpsso-um' );
 _x( 'Update Manager', 'lib file description', 'wpsso-um' );
 _x( 'WPSSO Update Manager', 'plugin name', 'wpsso-um' );
