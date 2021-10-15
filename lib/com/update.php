65b5cd18659b0ac1dfddfaba626818f43973fa93
JS Morisset <jsm@surniaulula.com>
2021-10-15 16:23:10 -0700

    v4.7.0

--- lib/com/update.php
+++ lib/com/update.php
@@ -36,7 +36,7 @@ if ( ! class_exists( 'SucomUpdate' ) ) {
 		private $sched_hours   = 24;
 		private $sched_name    = 'every24hours';
 
-		private static $api_version = '4.6.2';
+		private static $api_version = '4.7.0';
 		private static $upd_config  = array();
 		private static $offer_fname = 'offer-update.txt';
 
