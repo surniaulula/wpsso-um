5acf8fd68de3cae66e73bfd2227b025321b6a2a1
JS Morisset <jsm@surniaulula.com>
2021-11-13 20:35:29 -0800

    v4.10.1-dev.3

--- gettext/gettext-lib-config.php
+++ gettext/gettext-lib-config.php
@@ -11,3 +11,4 @@ _x( 'Update Manager', 'lib file description', 'wpsso-um' );
 _x( 'Update Manager', 'lib file description', 'wpsso-um' );
 _x( 'WPSSO Update Manager', 'plugin name', 'wpsso-um' );
 _x( 'Update Manager for the WPSSO Core Premium plugin.', 'plugin description', 'wpsso-um' );
+_x( 'WPSSO Core', 'plugin name', 'wpsso-um' );
