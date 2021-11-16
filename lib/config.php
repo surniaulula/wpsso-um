4e9423aeca681f67bd3067e8da4443cd4caba9aa
JS Morisset <jsm@surniaulula.com>
2021-11-16 12:50:29 -0800

    v4.10.1

--- lib/config.php
+++ lib/config.php
@@ -18,7 +18,7 @@ if ( ! class_exists( 'WpssoUmConfig' ) ) {
 			'plugin' => array(
 				'wpssogmf' => array(			// Plugin acronym.
 					'short'       => 'WPSSO GMF',	// Short plugin name.
-					'name'        => 'WPSSO Google Merchant Feeds',
+					'name'        => 'WPSSO Google Merchant Feeds XML',
 					'desc'        => 'Google Merchant Feeds for WooCommerce, Easy Digital Downloads, and Custom Products (WPSSO Core Premium Required).',
 					'slug'        => 'wpsso-google-merchant-feed',
 					'base'        => 'wpsso-google-merchant-feed/wpsso-google-merchant-feed.php',
