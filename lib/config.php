10dc35bd8c3843f0ee3153806edfc84b2abae15b
JS Morisset <jsm@surniaulula.com>
2022-01-02 21:30:26 -0800

    v4.10.3-dev.1

--- lib/config.php
+++ lib/config.php
@@ -16,6 +16,11 @@ if ( ! class_exists( 'WpssoUmConfig' ) ) {
 
 		public static $cf = array(
 			'plugin' => array(
+				'wpssojson' => array(			// Plugin acronym.
+					'url' => array(
+						'update' => 'https://wpsso.com/extend/plugins/wpsso-schema-json-ld/update/',
+					),
+				),
 				'wpssogmf' => array(			// Plugin acronym.
 					'short'       => 'WPSSO GMF',	// Short plugin name.
 					'name'        => 'WPSSO Google Merchant Feeds XML',
@@ -135,7 +140,7 @@ if ( ! class_exists( 'WpssoUmConfig' ) ) {
 					),
 				),
 				'wpssoum' => array(			// Plugin acronym.
-					'version'     => '4.10.2',	// Plugin version.
+					'version'     => '4.10.3-dev.1',	// Plugin version.
 					'opt_version' => '8',		// Increment when changing default option values.
 					'short'       => 'WPSSO UM',	// Short plugin name.
 					'name'        => 'WPSSO Update Manager',
