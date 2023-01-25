06a85a88a55b9abe7f77b1f37d0bca25e896459b
JS Morisset <jsm@surniaulula.com>
2023-01-25 03:57:13 -0800

    v4.14.1-dev.5

--- lib/config.php
+++ lib/config.php
@@ -77,7 +77,7 @@ if ( ! class_exists( 'WpssoUmConfig' ) ) {
 					),
 				),
 				'wpssoum' => array(			// Plugin acronym.
-					'version'     => '4.14.1-dev.4',	// Plugin version.
+					'version'     => '4.14.1-dev.5',	// Plugin version.
 					'opt_version' => '8',		// Increment when changing default option values.
 					'short'       => 'WPSSO UM',	// Short plugin name.
 					'name'        => 'WPSSO Update Manager',
