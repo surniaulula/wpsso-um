6a7dda81de6917c2bec2eec6a60bfce1c548c85d
JS Morisset <jsm@surniaulula.com>
2023-02-04 08:12:12 -0800

    v4.14.1

--- wpsso-um.php
+++ wpsso-um.php
@@ -69,6 +69,9 @@ if ( ! class_exists( 'WpssoUm' ) ) {
 			load_plugin_textdomain( 'wpsso-um', false, 'wpsso-um/languages/' );
 		}
 
+		/*
+		 * Called by Wpsso->set_objects which runs at init priority 10.
+		 */
 		public function init_objects() {
 
 			$this->p =& Wpsso::get_instance();
