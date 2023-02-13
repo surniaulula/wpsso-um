8353a4eacafe2570de711cf638c378c5d7fbdfa6
JS Morisset <jsm@surniaulula.com>
2023-02-13 06:09:25 -0800

    v4.15.0-dev.5

--- lib/com/update-util-wp.php
+++ lib/com/update-util-wp.php
@@ -148,7 +148,7 @@ if ( ! class_exists( 'SucomUpdateUtilWP' ) ) {
 		}
 
 		/*
-		 * Temporarily disable filter and action hooks before calling get_option(), update_option(), and delete_option().
+		 * Temporarily disable filters and actions hooks before calling get_option(), update_option(), and delete_option().
 		 */
 		public static function raw_do_option( $action, $opt_name, $value = null, $default = false ) {
 
