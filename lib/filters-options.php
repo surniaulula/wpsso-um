b82c119553f1ec30fd2a8ed895f58eb243a1fe5e
JS Morisset <jsm@surniaulula.com>
2023-08-07 18:00:14 -0700

    v4.18.0-dev.1

--- lib/filters-options.php
+++ lib/filters-options.php
@@ -26,26 +26,38 @@ if ( ! class_exists( 'WpssoUmFiltersOptions' ) ) {
 			$this->a =& $addon;
 
 			$this->p->util->add_plugin_filters( $this, array(
-				'save_setting_options' => 3,
-				'get_defaults'         => 1,	// Option defaults.
-				'get_site_defaults'    => 1,	// Site option defaults.
-				'option_type'          => 2,
+				'save_setting_options'  => 3,
+				'save_settings_options' => 3,
+				'get_defaults'          => 1,	// Option defaults.
+				'get_site_defaults'     => 1,	// Site option defaults.
+				'option_type'           => 2,
 			) );
 		}
 
 		/*
-		 * The 'wpsso_save_setting_options' filter is applied by WpssoOptions->save_options(),
-		 * WpssoAdmin->registered_setting_sanitation(), and WpssoAdmin->save_site_options().
-		 *
-		 * $upgrading will be true when the options version, not the plugin version, is being upgraded.
+		 * Deprecated since WPSSO Core v15.19.0.
+		 */
+		public function filter_save_setting_options( array $opts, $network, $upgrading ) {
+
+			_deprecated_function( __METHOD__ . '()', '2023/08/07',
+				$replacement = 'WpssoUmFiltersOptions::filter_save_settings_options()' );	// Deprecation message.
+
+			return $this->filter_save_settings_options( $opts, $network, $upgrading );
+		}
+
+		/*
+		 * The 'wpsso_save_settings_options' filter is applied by WpssoOptions->save_options(),
+		 * WpssoAdmin->settings_sanitation(), and WpssoAdmin->save_site_settings().
 		 *
 		 * $network is true if we're saving the multisite network settings.
+		 *
+		 * $upgrading will be true when the options version, not the plugin version, is being upgraded.
 		 *
 		 * Check for Authentication ID changes, and if the submitted values are different, refresh the update manager
 		 * config and force an update check. If the saved version string is different, then just refresh the update manager
 		 * config.
 		 */
-		public function filter_save_setting_options( array $opts, $network, $upgrading ) {
+		public function filter_save_settings_options( array $opts, $network, $upgrading ) {
 
 			if ( $network ) {
 
--- wpsso-um.php
+++ wpsso-um.php
@@ -15,7 +15,7 @@
  * Requires PHP: 7.2.34
  * Requires At Least: 5.5
  * Tested Up To: 6.3.0
- * Version: 4.17.1
+ * Version: 4.18.0-dev.1
  *
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
  *
