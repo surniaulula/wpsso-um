4434e6989169099eabcb3ce3b6b0e3383d28a07e
jsmoriss <jsm@surniaulula.com>
2024-10-30 19:01:25 +0000

    v7.2.2-b.7

--- wpsso-um.php
+++ wpsso-um.php
@@ -15,7 +15,7 @@
  * Requires PHP: 7.4.33
  * Requires At Least: 5.9
  * Tested Up To: 6.7.0
- * Version: 7.2.2-b.6
+ * Version: 7.2.2-b.7
  *
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
  *
